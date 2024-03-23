2bc82a313a003f4c4e8c0a488df0bf957f133e1b
Swarnava <swarnavamudi22@gmail.com>
2024-03-23 22:06:43 +0530

    Ts data types

---
+++ Basic Code /Ts type/Ts types.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
+var greetigns = "Ishaan";
+console.log(greetigns);
+// number 
+var userId;
+userId = 33544;
+// userId = 2345.5
+// boolean
+var isLoggedIn = false;
+// any
+var hero;
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
---
+++ Basic Code /Ts type/Ts types.ts
@@ -0,0 +1,22 @@
+// string
+let greetigns: string = "Ishaan"
+console.log(greetigns);
+
+// number 
+let userId: number
+userId = 33544
+// userId = 2345.5
+
+// boolean
+let isLoggedIn: boolean = false
+
+// any
+ let hero;
+
+
+ function addTwo(num: number){
+    return num + 2
+ }
+ addTwo(5)
+
+export{}
