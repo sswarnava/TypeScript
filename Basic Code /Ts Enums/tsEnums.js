dc1c5cea36305a3e8294c8210937f778fea5e9cb
Swarnava <swarnavamudi22@gmail.com>
2024-03-24 19:54:56 +0530

    add ts enums

---
+++ Basic Code /Ts Enums/tsEnums.js
@@ -0,0 +1,28 @@
+"use strict";
+// An enum is a special "class" that represents a group of constants (unchangeable variables).
+Object.defineProperty(exports, "__esModule", { value: true });
+//   -- Numeric Enums - Fully Initialized
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+    StatusCodes[StatusCodes["Accepted"] = 202] = "Accepted";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+})(StatusCodes || (StatusCodes = {}));
+// logs 404
+console.log(StatusCodes.NotFound);
+// logs 200
+console.log(StatusCodes.Success);
+// -- String Enums
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections["North"] = "North";
+    CardinalDirections["East"] = "East";
+    CardinalDirections["South"] = "South";
+    CardinalDirections["West"] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+;
+// logs "North"
+console.log(CardinalDirections.North);
+// logs "West"
+console.log(CardinalDirections.West);
---
+++ Basic Code /Ts Enums/tsEnums.ts
@@ -0,0 +1,28 @@
+// An enum is a special "class" that represents a group of constants (unchangeable variables).
+
+//   -- Numeric Enums - Fully Initialized
+enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400
+  }
+  // logs 404
+  console.log(StatusCodes.NotFound);
+  // logs 200
+  console.log(StatusCodes.Success);
+
+  // -- String Enums
+
+  enum CardinalDirections {
+    North = 'North',
+    East = "East",
+    South = "South",
+    West = "West"
+  };
+  // logs "North"
+  console.log(CardinalDirections.North);
+  // logs "West"
+  console.log(CardinalDirections.West);
+
+  export {}
